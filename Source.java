a696ac2c0f15a5246d7e05a164cac4e48e4c75f2
mm-fiserv <147175865+mm-fiserv@users.noreply.github.com>
2023-11-03 13:19:35 -0400

    Add files via upload

--- Source.java
+++ Source.java
@@ -1,34 +1,50 @@
-class Node<T> {
-  T val;
-  Node<T> next;
+import java.util.ArrayList;
+class Node<T>
+{
+    T val;
+    Node<T> next;
 
-  public Node(T val) {
-    this.val = val;
-    this.next = null;
-  }
+    public Node(T val)
+    {
+        this.val = val;
+        this.next = null;
+    }
 }
 
-class Source {
-  public static int sumList(Node<Integer> head) {
-    // todo
-    return;
-  }
+class Source
+{
+    public static int sumList(Node<Integer> head)
+    {
+        ArrayList<Integer> values = new ArrayList<>();
+        Node<Integer> current = head;
 
-  public static void main(String[] args) {
-    Node<Integer> a = new Node<>(2);
-    Node<Integer> b = new Node<>(8);
-    Node<Integer> c = new Node<>(3);
-    Node<Integer> d = new Node<>(-1);
-    Node<Integer> e = new Node<>(7);
+        int sum=0;
+        while (current != null)
+        {
+            sum+= current.val;
+            current = current.next;
+        }
+        return sum;
+    }
 
-    a.next = b;
-    b.next = c;
-    c.next = d;
-    d.next = e;
+    public static void main(String[] args)
+    {
+        Node<Integer> a = new Node<>(2);
+        Node<Integer> b = new Node<>(8);
+        Node<Integer> c = new Node<>(3);
+        Node<Integer> d = new Node<>(-1);
+        Node<Integer> e = new Node<>(7);
 
-    // 2 -> 8 -> 3 -> -1 -> 7
-    
-    System.out.println("Sum of my list: ");
-    System.out.println(Source.sumList(a));
-  }
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+        // 2 -> 8 -> 3 -> -1 -> 7
+
+        System.out.println("Sum of my list: ");
+        System.out.println(Source.sumList(a));
+    }
 }
+
+
